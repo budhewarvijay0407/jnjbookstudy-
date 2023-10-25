82e5f86d1efcd0cbc787b26d866968516afef453
budhewarvijay0407 <70945959+budhewarvijay0407@users.noreply.github.com>
2023-10-25 13:21:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Importing all the necessary language 
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
